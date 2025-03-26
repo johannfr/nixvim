ce8452a2a6bb5e9bb0fd06d7a22a8e37432d6f92
Jóhann Friðriksson <johann.fridriksson@gmail.com>
2025-03-26 17:21:02 +0000

    Adjusting some Python formatting settings.

--- config/plugins/fmt.nix
+++ config/plugins/fmt.nix
@@ -3,11 +3,16 @@
     enable = true;
     settings = {
       formatters_by_ft = {
-        python = ["isort" "black"];
+        python = {
+          __unkeyed-1 = "isort";
+          __unkeyed-2 = "black";
+          timeout_ms = 10000;
+          stop_after_first = false;
+        };
       };
       format_on_save = {
         lspFallback = true;
-        timeoutMs = 2000;
+        timeout_ms = 10000;
       };
     };
   };

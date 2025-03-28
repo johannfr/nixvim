5cf818df3a9deedc26817fa722de38ba0d14d76b
Jóhann Friðriksson <johann.fridriksson@gmail.com>
2025-03-28 15:34:03 +0000

    Adding my astyle-within-Docker thing for C++ formatting.  Please enter the commit message for your changes. Lines starting

--- config/plugins/fmt.nix
+++ config/plugins/fmt.nix
@@ -3,6 +3,9 @@
     enable = true;
     settings = {
       formatters_by_ft = {
+        cpp = [
+          "dockerastyle"
+        ];
         python = {
           __unkeyed-1 = "isort";
           __unkeyed-2 = "ruff_format";
@@ -13,6 +16,11 @@
       format_on_save = {
         lspFallback = true;
         timeout_ms = 10000;
+      };
+      formatters = {
+        dockerastyle = {
+          command = "/home/jof/bin/dockerastyle.sh";
+        };
       };
     };
   };

f6d11fd831ae98adb68ffd7b8db2fed6b1183fb7
Jóhann Friðriksson <johann.fridriksson@gmail.com>
2025-07-08 16:32:49 +0000

    Remove this pesky tab for next item business.

--- config/plugins/cmp.nix
+++ config/plugins/cmp.nix
@@ -43,9 +43,7 @@
             "<Tab>" = ''
               cmp.mapping(
                 function(fallback)
-                  if cmp.visible() then
-                    cmp.select_next_item()
-                  elseif require("luasnip").expand_or_locally_jumpable() then
+                  if require("luasnip").expand_or_locally_jumpable() then
                     require("luasnip").expand_or_jump()
                   elseif has_words_before() then
                     cmp.complete()

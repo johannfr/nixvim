0321eac16d8dccf844686c5146a0dea1bd717f5c
Jóhann Friðriksson <johann.fridriksson@gmail.com>
2025-10-13 14:17:21 +0000

    Added mini.move

--- config/plugins/other.nix
+++ config/plugins/other.nix
@@ -23,4 +23,23 @@
   plugins.copilot-chat = {
     enable = true;
   };
+
+  plugins.mini = {
+    enable = true;
+    modules = {
+      move = {
+        mappings = {
+          left = "<M-Left>";
+          right = "<M-Right>";
+          down = "<M-Down>";
+          up = "<M-Up>";
+          line_left = "<M-Left>";
+          line_right = "<M-Right>";
+          line_down = "<M-Down>";
+          line_up = "<M-Up>";
+
+        };
+      };
+    };
+  };
 }

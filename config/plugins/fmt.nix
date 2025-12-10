c380c28153293a1bf6b4118f64f8b98c9b27a1d5
Jóhann Friðriksson <johann.fridriksson@gmail.com>
2025-12-10 14:27:15 +0000

    Condigtion on dockerastyle and fallback on clang-format

--- config/plugins/fmt.nix
+++ config/plugins/fmt.nix
@@ -5,13 +5,13 @@
       formatters_by_ft = {
         cpp = [
           "dockerastyle"
+          "clang-format"
         ];
-        python = {
-          __unkeyed-1 = "isort";
-          __unkeyed-2 = "ruff_format";
-          timeout_ms = 10000;
-          stop_after_first = false;
-        };
+
+        python = [
+          "isort"
+          "ruff_format"
+        ];
       };
       format_on_save = {
         lspFallback = true;
@@ -20,8 +20,36 @@
       formatters = {
         dockerastyle = {
           command = "/home/jof/vinna/docker/isds/dockerastyle.sh";
+          condition = { __raw = ''
+            function(self, ctx)
+              local allowed_projects = { "isds", "ice", "netrrr" }
+
+              local file_dir = vim.fs.dirname(ctx.filename)
+              local cmd = { "git", "-C", file_dir, "rev-parse", "--show-toplevel" }
+              local git_root = vim.fn.system(cmd)
+
+              if vim.v.shell_error ~= 0 then
+                return false
+              end
+
+              local project_name = vim.fs.basename(vim.trim(git_root))
+
+              for _, name in ipairs(allowed_projects) do
+                if project_name == name then
+                  return true
+                end
+              end
+
+              return false
+            end
+        ''; };
         };
       };
     };
   };
+  extraConfigLua = ''
+    require("conform").formatters_by_ft.cpp["stop_after_first"] = true
+    require("conform").formatters_by_ft.python["stop_after_first"] = false
+    require("conform").formatters_by_ft.python["timeout_ms"] = 10000
+  '';
 }
